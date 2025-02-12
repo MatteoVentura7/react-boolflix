48486400265a7a7ebe5304c32e39223208b0260b
MatteoVentura <matteo.ventura18@gmail.com>
2025-02-12 16:40:34 +0100

    add : hover card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -19,7 +19,28 @@ export default function Main() {
               <img
                 src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}
                 alt="nuul"
+                style={{
+                  obejctFit: "cover",
+                  width: "100%",
+                  height: "100%",
+                }}
               />
+              {/* <div>
+                {" "}
+                title: {movie.title} <br /> title original:{" "}
+                {movie.original_title} <br />{" "}
+                {langs.includes(movie.original_language) ? (
+                  <img
+                    src={`/img/${movie.original_language}.png`}
+                    alt={movie.original_language}
+                  />
+                ) : (
+                  <p>Lingua: {movie.original_language}</p>
+                )}{" "}
+                <br />
+                Voto: {Math.ceil(movie.vote_average / 2)}
+                <i class="fa-solid fa-star"></i>
+              </div> */}
             </div>
           ))}
         </div>
